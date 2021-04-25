6e91209e3c12e6cd2028300aec27e17c1c6eee27
Khiemnd <khiemnd777@gmail.com>
2021-04-25 09:41:59 +0700

    supports SEO component

--- api/post-items/controllers/post-items.js
+++ api/post-items/controllers/post-items.js
@@ -1,8 +1,13 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async seo(ctx) {
+    const entities = await strapi.services["post-items"].find({Slug: ctx.params.slug});
+    return entities ? entities[0] && entities[0].Seo : null;
+  },
+};
