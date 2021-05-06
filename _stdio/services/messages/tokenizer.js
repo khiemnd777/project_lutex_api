16cbf8d41ec26a5e51a9935967bef2b794dea8a1
DESKTOP-FAKMJLK\User <khiemnd777@gmail.com>
2021-05-07 01:19:07 +0700

    tokenizer and token templates

---
+++ _stdio/services/messages/tokenizer.js
@@ -0,0 +1,25 @@
+const { isArray, toSanitizedModel, toSanitizedModels } = require("../../shared/utils");
+
+module.exports = {
+  Website: {
+    async Name() {
+      return await strapi.query("environment").find().Name;
+    },
+  },
+  Post: {
+    async FirstPost() {
+      const postItems = await strapi
+        .query("post-items")
+        .find({ _sort: "createdAt:desc", _start: 0, _limit: 1 });
+      return !!postItems && isArray(postItems)
+        ? toSanitizedModel(postItems[0], strapi.models["post-items"])
+        : null;
+    },
+    async NewestPosts(start, limit) {
+      const postItems = await strapi.query("post-items").find({ _sort: "createdAt:desc", _start: start, _limit: limit });
+      return !!postItems && isArray(postItems)
+        ? toSanitizedModels(postItems, strapi.models["post-items"])
+        : [];
+    }
+  },
+};
