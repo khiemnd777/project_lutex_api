03ce531a336d5703e6c55b8ab20e8f3f182e2ab2
DESKTOP-FAKMJLK\User <khiemnd777@gmail.com>
2021-05-08 10:00:41 +0700

    cron enabled

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  cron: { enabled: env.bool("CRON_ENABLED") },
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'f228183792b3b18ce7173b1dfac84503'),
+      secret: env("ADMIN_JWT_SECRET", "f228183792b3b18ce7173b1dfac84503"),
     },
   },
 });
