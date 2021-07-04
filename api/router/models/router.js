3911198d8070f432cc59c92422f4e579f0e787b5
Khiemnd <khiemnd777@gmail.com>
2021-07-04 12:06:17 +0700

    fix router widgets

--- api/router/models/router.js
+++ api/router/models/router.js
@@ -1,8 +1,19 @@
 'use strict';
 
+const { syncWidgetWithEmptyFields } = require('../../../_stdio/services/widget/widget-utils');
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles:{
+    async beforeCreate(data){
+      await syncWidgetWithEmptyFields(data.Widgets);
+    },
+    async beforeUpdate(params, data){
+      await syncWidgetWithEmptyFields(data.Widgets);
+    },
+  }
+};
