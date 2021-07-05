e79b4897dcc4d67ffcd31b792f0a0f1fc0095db8
khiem.nguyen <knguyen20@wellbarrier.slb.com>
2021-07-06 00:38:40 +0700

    widget update on-demand

--- _stdio/services/widget/widget-utils.js
+++ _stdio/services/widget/widget-utils.js
@@ -3,44 +3,47 @@ const { isArray } = require("../../shared/utils");
 const syncWidgetWithEmptyFields = async (widgets) => {
   if (isArray(widgets)) {
     await Promise.all(
-      widgets.map(async (widget) => {
-        if (widget.widget) {
-          const matchedWidget = await strapi
-            .query("widget")
-            .findOne({ id: widget.widget });
-          if (matchedWidget) {
-            // Name
-            if (!widget.Name) {
-              matchedWidget.Name && (widget.Name = matchedWidget.Name);
-            }
-            // Configuration Name
-            if (!widget.ConfigurationName) {
-              matchedWidget.ConfigurationName &&
-                (widget.ConfigurationName = matchedWidget.ConfigurationName);
-            }
-            // Add or Update Parameters
-            if (isArray(matchedWidget.Parameters)) {
-              if (!isArray(widget.Parameters)) {
-                widget.Parameters = [];
+      widgets
+        .filter((widget) => widget.Update)
+        .map(async (widget) => {
+          widget.Update = false;
+          if (widget.widget) {
+            const matchedWidget = await strapi
+              .query("widget")
+              .findOne({ id: widget.widget });
+            if (matchedWidget) {
+              // Name
+              if (!widget.Name) {
+                matchedWidget.Name && (widget.Name = matchedWidget.Name);
               }
-              matchedWidget.Parameters.forEach((p) => {
-                const foundWp = widget.Parameters.find(
-                  (wp) => wp.Name === p.Name
-                );
-                if (foundWp && !foundWp.Value) {
-                  foundWp.Value = p.Value;
+              // Configuration Name
+              if (!widget.ConfigurationName) {
+                matchedWidget.ConfigurationName &&
+                  (widget.ConfigurationName = matchedWidget.ConfigurationName);
+              }
+              // Add or Update Parameters
+              if (isArray(matchedWidget.Parameters)) {
+                if (!isArray(widget.Parameters)) {
+                  widget.Parameters = [];
                 }
-                if (!foundWp) {
-                  widget.Parameters.push({
-                    Name: p.Name,
-                    Value: p.Value,
-                  });
-                }
-              });
+                matchedWidget.Parameters.forEach((p) => {
+                  const foundWp = widget.Parameters.find(
+                    (wp) => wp.Name === p.Name
+                  );
+                  if (foundWp && !foundWp.Value) {
+                    foundWp.Value = p.Value;
+                  }
+                  if (!foundWp) {
+                    widget.Parameters.push({
+                      Name: p.Name,
+                      Value: p.Value,
+                    });
+                  }
+                });
+              }
             }
           }
-        }
-      })
+        })
     );
   }
 };
