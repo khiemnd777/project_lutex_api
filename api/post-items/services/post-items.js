a8c0834f242597d27c2e2b6fb204cf6f2a591504
khiem.nguyen <knguyen20@wellbarrier.slb.com>
2021-07-12 23:09:11 +0700

    init search feature

--- api/post-items/services/post-items.js
+++ api/post-items/services/post-items.js
@@ -1,8 +1,16 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  async search(query, limit, start) {
+    console.log(`query ${query}, limit ${limit}, start ${start}`);
+    const result = await strapi
+      .query("post-items")
+      .search({ _q: query, _limit: limit, _start: start });
+      return result;
+  },
+};
