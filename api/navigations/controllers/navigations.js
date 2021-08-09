604ff9ce299285aed43557940c8fdb0c651f3a5d
khiem.nguyen <knguyen20@wellbarrier.slb.com>
2021-08-10 02:15:35 +0700

    some fix]

--- api/navigations/controllers/navigations.js
+++ api/navigations/controllers/navigations.js
@@ -1,8 +1,17 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async findByName(ctx) {
+    const entities = await strapi.query('navigations')
+      .model
+      .find({Name: ctx.params.name})
+      .select('Children');
+    const models = entities;
+    return models[0];
+  },
+};
