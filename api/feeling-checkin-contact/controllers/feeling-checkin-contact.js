1fadcdf85fa54c6f23ee4599d535d4aee31b7e13
khiem.nguyen <knguyen20@wellbarrier.slb.com>
2021-10-16 12:50:26 +0700

    adding contact to checkin form

--- api/feeling-checkin-contact/controllers/feeling-checkin-contact.js
+++ api/feeling-checkin-contact/controllers/feeling-checkin-contact.js
@@ -1,8 +1,20 @@
 'use strict';
 
+const { toSanitizedModel } = require('../../../_stdio/shared/utils');
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async addContact(ctx){
+    const body =
+      "string" === typeof ctx.request.body
+        ? parseBody(ctx.request.body)
+        : ctx.request.body;
+    const service = strapi.services['feeling-checkin-contact'];
+    const entity = await service.addContact(body.contact, body.answers);
+    return entity && toSanitizedModel(entity, strapi.models['feeling-checkin-contact']);
+  }
+};
--- api/feeling-checkin-contact/services/feeling-checkin-contact.js
+++ api/feeling-checkin-contact/services/feeling-checkin-contact.js
@@ -1,8 +1,50 @@
-'use strict';
+"use strict";
+
+const { isArray } = require("../../../_stdio/shared/utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  async addContact(contact, answers) {
+    /**
+     * contact: {
+     *  FullName: string;
+     *  Email: string;
+     *  PhoneNumber: string;
+     *  Content: string;
+     * }
+     * 
+     * answers: {
+     *   question: string;
+     *   answers: {
+     *     value: string;
+     *   }[]
+     * }[]
+     */
+    const model = {
+      FullName: contact.FullName,
+      Email: contact.Email,
+      PhoneNumber: contact.PhoneNumber,
+      Content: contact.Content,
+    };
+    if (isArray(answers) && answers.length) {
+      model.FeelingCheckinData = JSON.parse(JSON.stringify(answers));
+    }
+    try {
+      const validModel = await strapi.entityValidator.validateEntityCreation(
+        strapi.models["feeling-checkin-contact"],
+        model
+      );
+      const entry = await strapi
+        .query("feeling-checkin-contact")
+        .create(validModel);
+      return entry;
+    } catch (ex) {
+      strapi.log.debug(ex);
+      throw ex;
+    }
+  },
+};
